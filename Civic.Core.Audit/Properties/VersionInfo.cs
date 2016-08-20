04cdc69a536040e4928b2bbb978b6131ab6ecd95
dotyc <dotyc@civicinc.com>
2016-08-20 15:22:08 -0500

    make it compile need to remove caching dep on data

---
+++ Civic.Core.Audit/Properties/VersionInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion("1.21.0.0")]
+[assembly: AssemblyFileVersion("1.21.0.0")]
+[assembly: AssemblyCompany("Civic Engineering & IT")]
+[assembly: AssemblyCopyright("Copyright © 2016 Civic Engineering & IT")]
