3c4b38d269a3caec120bbdaa3818732d9f035814
dotyc <dotyc@civicinc.com>
2016-08-22 01:24:26 -0500

    fix nuget build

--- Civic.Core.Audit/Properties/VersionInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-
-[assembly: AssemblyVersion("1.21.0.0")]
-[assembly: AssemblyFileVersion("1.21.0.0")]
-[assembly: AssemblyCompany("Civic Engineering & IT")]
-[assembly: AssemblyCopyright("Copyright © 2016 Civic Engineering & IT")]
