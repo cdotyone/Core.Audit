30a56371e3eb65775f56c7373a5620ba6edc72f5
Chris Doty <dotyc@civicinc.com>
2019-01-13 21:20:45 -0600

    fix: copyright year

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("1.26.0.0")]
 [assembly: AssemblyFileVersion("1.26.6.5078")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
-[assembly: AssemblyCopyright("Copyright © 2017 Civic Engineering & IT")]
+[assembly: AssemblyCopyright("Copyright © 2019 Civic Engineering & IT")]
