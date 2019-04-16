05f2b9b7b360335218149cad2858aa4d1af89b75
ci <jenkins@CivicInc.local>
2019-04-15 22:29:48 -0500

    [ci skip]

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.1.151")]
+[assembly: AssemblyFileVersion("2.0.2.194")]
 [assembly: AssemblyCompany("Chris Doty")]
 [assembly: AssemblyCopyright("Copyright © 2019 Chris Doty")]
