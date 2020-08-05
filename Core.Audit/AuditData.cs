09c70ecb072ad9c3c584d25f2930117a291b5b09
Chris Doty <dotyc@civicinc.com>
2020-08-04 21:51:48 -0500

    fix: add ouid for audit

--- Core.Audit/AuditData.cs
+++ Core.Audit/AuditData.cs
@@ -6,43 +6,66 @@ using System.Globalization;
 using Core.Data;
 using Core.Logging;
 using Newtonsoft.Json;
-using IDbCommand = Core.Data.IDbCommand;
 
 namespace Core.Audit
 {
     public class AuditData
     {
-
-        public static AuditLog GetAuditLog(int id, IDBConnection database)
+        public static AuditLog GetAuditLog(Int32 id, IDBConnection database)
         {
             Debug.Assert(database != null);
-            var auditLogReturned = new AuditLog();
+            var AuditLogReturned = new AuditLog();
 
             using (var command = database.CreateStoredProcCommand("core", "usp_AuditLogGet"))
             {
                 command.AddInParameter("@id", id);
                 command.ExecuteReader(dataReader =>
+                {
+                    if (populateAuditLog(AuditLogReturned, dataReader))
                     {
-                        if (populateAuditLog(auditLogReturned, dataReader))
-                        {
-                            auditLogReturned.ID = id;
-                        }
-                        else auditLogReturned = null;
-                    });
+                        AuditLogReturned.ID = id;
+                    }
+                    else AuditLogReturned = null;
+                });
             }
 
-            return auditLogReturned;
+            return AuditLogReturned;
+        }
+
+        public static AuditLog GetAuditLog(string ouid, string uid, IDBConnection database)
+        {
+            Debug.Assert(database != null);
+            var AuditLogReturned = new AuditLog();
+
+            using (var command = database.CreateStoredProcCommand("core", "usp_AuditLogGet"))
+            {
+                command.AddInParameter("@ouid", ouid);
+                command.AddInParameter("@uid", uid);
+                command.ExecuteReader(dataReader =>
+                {
+                    if (!populateAuditLog(AuditLogReturned, dataReader))
+                        AuditLogReturned = null;
+                });
+            }
+
+            return AuditLogReturned;
         }
 
         public static List<AuditLog> GetPagedAuditLog(int skip, ref int count, bool retCount, string filterBy, string orderBy, IDBConnection database)
+        {
+            return GetPagedAuditLog(null, skip, ref count, retCount, filterBy, orderBy, database);
+        }
+
+        public static List<AuditLog> GetPagedAuditLog(string ouid, int skip, ref int count, bool retCount, string filterBy, string orderBy, IDBConnection database)
         {
             Debug.Assert(database != null);
             var list = new List<AuditLog>();
 
             using (var command = database.CreateStoredProcCommand("core", "usp_AuditLogGetFiltered"))
             {
+                command.AddInParameter("@ouid", ouid);
                 command.AddInParameter("@skip", skip);
-                command.AddInParameter("@retCount", retCount);
+                command.AddInParameter("@retcount", retCount);
                 if (!string.IsNullOrEmpty(filterBy)) command.AddInParameter("@filterBy", filterBy);
                 command.AddInParameter("@orderBy", orderBy);
                 command.AddParameter("@count", ParameterDirection.InputOutput, count);
@@ -70,23 +93,27 @@ namespace Core.Audit
             {
                 buildAuditLogCommandParameters(auditLog, command, useLocalTime, true);
                 command.ExecuteNonQuery();
+
+                if (command.GetOutParameter("@id").Value == null) return -1;
                 auditLog.ID = Int32.Parse(command.GetOutParameter("@id").Value.ToString());
                 return auditLog.ID.Value;
             }
         }
 
-        private static void buildAuditLogCommandParameters(AuditLog auditLog, IDbCommand command, bool useLocalTime, bool addRecord)
+        private static void buildAuditLogCommandParameters(AuditLog auditLog, IDBCommand command, bool useLocalTime, bool addRecord)
         {
             if (addRecord) command.AddParameter("@id", ParameterDirection.InputOutput, auditLog.ID);
             else command.AddInParameter("@id", auditLog.ID);
+            command.AddInParameter("@UID", auditLog.UID);
+            command.AddInParameter("@OUID", auditLog.OUID);
             command.AddInParameter("@trackingUID", auditLog.TrackingUID);
-            command.AddInParameter("@entityCode", auditLog.EntityCode);
-            command.AddInParameter("@entityKeys", auditLog.EntityKeys);
+            command.AddInParameter("@entitycode", auditLog.EntityCode);
+            command.AddInParameter("@entitykeys", auditLog.EntityKeys);
             command.AddInParameter("@clientMachine", auditLog.ClientMachine);
-            command.AddInParameter("@relatedEntityCode", auditLog.RelatedEntityCode);
-            command.AddInParameter("@relatedEntityKeys", auditLog.RelatedEntityKeys);
+            command.AddInParameter("@relatedentitycode", auditLog.RelatedEntityCode);
+            command.AddInParameter("@relatedentitykeys", auditLog.RelatedEntityKeys);
             command.AddInParameter("@action", auditLog.Action);
-            if(auditLog.Created.HasValue) command.AddInParameter("@created", auditLog.Created);   
+            if (auditLog.Created.HasValue) command.AddInParameter("@created", auditLog.Created);
             else command.AddInParameter("@created", useLocalTime ? DateTime.Now : DateTime.UtcNow);
             command.AddInParameter("@success", auditLog.Success ? "Y" : "N");
             command.AddInParameter("@processed", auditLog.Processed);
@@ -104,15 +131,26 @@ namespace Core.Audit
 
             auditLog.ID = dataReader["ID"] != null && !(dataReader["ID"] is DBNull) ? Int32.Parse(dataReader["ID"].ToString()) : 0;
             auditLog.TrackingUID = dataReader["TrackingUID"] != null && !string.IsNullOrEmpty(dataReader["TrackingUID"].ToString()) ? dataReader["TrackingUID"].ToString() : string.Empty;
+
+            if (Configuration.AuditConfig.Current.UseOUID)
+            {
+                auditLog.UID = dataReader["UID"] != null && !string.IsNullOrEmpty(dataReader["UID"].ToString()) ? dataReader["UID"].ToString() : string.Empty;
+                auditLog.OUID = dataReader["OUID"] != null && !string.IsNullOrEmpty(dataReader["OUID"].ToString()) ? dataReader["OUID"].ToString() : string.Empty;
+            }
+
+
             auditLog.EntityCode = dataReader["EntityCode"] != null && !string.IsNullOrEmpty(dataReader["EntityCode"].ToString()) ? dataReader["EntityCode"].ToString() : string.Empty;
             auditLog.EntityKeys = dataReader["EntityKeys"] != null && !string.IsNullOrEmpty(dataReader["EntityKeys"].ToString()) ? dataReader["EntityKeys"].ToString() : string.Empty;
             auditLog.RelatedEntityCode = dataReader["RelatedEntityCode"] != null && !string.IsNullOrEmpty(dataReader["RelatedEntityCode"].ToString()) ? dataReader["RelatedEntityCode"].ToString() : string.Empty;
             auditLog.RelatedEntityKeys = dataReader["RelatedEntityKeys"] != null && !string.IsNullOrEmpty(dataReader["RelatedEntityKeys"].ToString()) ? dataReader["RelatedEntityKeys"].ToString() : string.Empty;
             auditLog.Action = dataReader["Action"] != null && !string.IsNullOrEmpty(dataReader["Action"].ToString()) ? dataReader["Action"].ToString() : string.Empty;
             auditLog.ClientMachine = dataReader["ClientMachine"] != null && !string.IsNullOrEmpty(dataReader["ClientMachine"].ToString()) ? dataReader["ClientMachine"].ToString() : string.Empty;
-            
+
+            string before = dataReader["Before"] != null && !string.IsNullOrEmpty(dataReader["Before"].ToString()) ? dataReader["Before"].ToString() : string.Empty;
+            string after = dataReader["After"] != null && !string.IsNullOrEmpty(dataReader["After"].ToString()) ? dataReader["After"].ToString() : string.Empty;
+
             var format = new CultureInfo("en-US").DateTimeFormat;
-            auditLog.Success = dataReader["Success"] != null && !(dataReader["Success"] is DBNull) && dataReader["Success"].ToString()=="Y";
+            auditLog.Success = dataReader["Success"] != null && !(dataReader["Success"] is DBNull) && dataReader["Success"].ToString() == "Y";
             if (!(dataReader["Created"] is DBNull)) auditLog.Created = DateTime.Parse(dataReader["Created"].ToString(), format, DateTimeStyles.AssumeLocal);
             if (!(dataReader["Recorded"] is DBNull)) auditLog.Recorded = DateTime.Parse(dataReader["Recorded"].ToString(), format, DateTimeStyles.AssumeLocal);
             auditLog.CreatedBy = dataReader["CreatedBy"] != null && !string.IsNullOrEmpty(dataReader["CreatedBy"].ToString()) ? dataReader["CreatedBy"].ToString() : string.Empty;
@@ -122,18 +160,24 @@ namespace Core.Audit
                 if (dataReader["Before"] != null)
                     auditLog.Before = JsonConvert.DeserializeObject<Dictionary<string, string>>(dataReader["Before"].ToString());
             }
-            catch
+            catch (Exception ex)
             {
-                Logger.LogError(LoggingBoundaries.DataLayer, "Audit.EntityCode={0} , Audit.EntityCode={1} : Failed to parse before json: {2}", auditLog.EntityCode, auditLog.EntityKeys, dataReader["Before"].ToString());
+                Logger.LogError(LoggingBoundaries.DataLayer, "Audit.entitycode={0} , Audit.entitycode={1} : Failed to parse before json: {2}", auditLog.EntityCode, auditLog.EntityKeys, dataReader["Before"].ToString());
             }
             try
             {
                 if (dataReader["After"] != null)
                     auditLog.After = JsonConvert.DeserializeObject<Dictionary<string, string>>(dataReader["After"].ToString());
             }
-            catch
+            catch (Exception ex)
             {
-                Logger.LogError(LoggingBoundaries.DataLayer, "Audit.EntityCode={0} , Audit.EntityCode={1} : Failed to parse after json: {2}", auditLog.EntityCode, auditLog.EntityKeys, dataReader["After"].ToString());
+                Logger.LogError(LoggingBoundaries.DataLayer, "Audit.entitycode={0} , Audit.entitycode={1} : Failed to parse after json: {2}", auditLog.EntityCode, auditLog.EntityKeys, dataReader["After"].ToString());
+            }
+
+            if (Configuration.AuditConfig.Current.UseOUID)
+            {
+                auditLog.UID = dataReader["UID"] != null && !string.IsNullOrEmpty(dataReader["UID"].ToString()) ? dataReader["UID"].ToString() : string.Empty;
+                auditLog.OUID = dataReader["OUID"] != null && !string.IsNullOrEmpty(dataReader["OUID"].ToString()) ? dataReader["OUID"].ToString() : string.Empty;
             }
 
             return true;
--- Core.Audit/Configuration/AuditConfig.cs
+++ Core.Audit/Configuration/AuditConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using Core.Audit.Providers;
 using Core.Configuration;
@@ -8,18 +9,22 @@ namespace Core.Audit.Configuration
     public class AuditConfig : NamedConfigurationElement
     {
         protected const string USE_LOCAL_TIME = "useLocalTime";
+        protected const string USE_OUID = "useOUID";
+
         private static CoreSection _coreConfig;
         private static AuditConfig _current;
         private string _default;
         private bool _useLocalTime;
+        private bool _useOUID;
 
         public AuditConfig(INamedElement element)
         {
-            if (element == null) element = new NamedConfigurationElement() { Name = SectionName };
+            if (element == null) element = new NamedConfigurationElement() {Name = SectionName};
             Children = element.Children;
             Attributes = element.Attributes;
             //Name = element.Name;
             _useLocalTime = Attributes.ContainsKey(USE_LOCAL_TIME) && bool.Parse(Attributes[USE_LOCAL_TIME]);
+            _useOUID = Attributes.ContainsKey(USE_OUID) && bool.Parse(Attributes[USE_OUID]);
         }
 
         /// <summary>
@@ -32,7 +37,9 @@ namespace Core.Audit.Configuration
                 if (_current != null) return _current;
 
                 if (_coreConfig == null) _coreConfig = CoreSection.Current;
-                _current = new AuditConfig(_coreConfig.Children.ContainsKey(SectionName) ? _coreConfig.Children[SectionName] : null);
+                _current = new AuditConfig(_coreConfig.Children.ContainsKey(SectionName)
+                    ? _coreConfig.Children[SectionName]
+                    : null);
                 return _current;
             }
         }
@@ -51,14 +58,34 @@ namespace Core.Audit.Configuration
         public string DefaultProvider
         {
             get { return _default; }
-            set { _default = value; Attributes[Constants.CONFIG_PROP_DEFAULTPROVIDER] = value; }
+            set
+            {
+                _default = value;
+                Attributes[Constants.CONFIG_PROP_DEFAULTPROVIDER] = value;
+            }
         }
 
         public bool UseLocalTime
         {
             get { return _useLocalTime; }
-            set { _useLocalTime = value; Attributes[USE_LOCAL_TIME] = value.ToString(); }
+            set
+            {
+                _useLocalTime = value;
+                Attributes[USE_LOCAL_TIME] = value.ToString();
+            }
         }
+
+        public bool UseOUID
+        {
+            get { return _useOUID; }
+            set
+            {
+                _useOUID = value;
+                Attributes[USE_OUID] = value.ToString();
+            }
+        }
+
+        private static object providerLock = new object();
 
         /// <summary>
         /// Gets the collection cache providers
@@ -67,21 +94,24 @@ namespace Core.Audit.Configuration
         {
             get
             {
-                if (_providers != null) return _providers;
-                if (Children.Count == 0)
+                /// Made thread safe.
+                lock (providerLock)
                 {
-                    Children["SqlAuditProvider"] = new AuditProviderElement(new SqlAuditProvider());
-                }
+                    if (_providers == null)
+                    {
+                        if (Children.Count == 0)
+                            Children["SqlAuditProvider"] = new AuditProviderElement(new SqlAuditProvider());
 
-                _providers = new ConcurrentDictionary<string, AuditProviderElement>();
-                foreach (var element in Children)
-                {
-                    _providers[element.Key.ToLowerInvariant()] = new AuditProviderElement(element.Value, this);
+                        _providers = new ConcurrentDictionary<string, AuditProviderElement>();
+                        foreach (var element in Children)
+                            _providers[element.Key.ToLowerInvariant()] = new AuditProviderElement(element.Value, this);
+                    }
                 }
 
                 return _providers;
             }
         }
+
         private ConcurrentDictionary<string, AuditProviderElement> _providers;
 
         /// <summary>
@@ -91,8 +121,16 @@ namespace Core.Audit.Configuration
         /// <returns>the list of providers</returns>
         public List<IAuditProvider> GetProvidersByModule(string moduleName)
         {
+            if (string.IsNullOrEmpty(moduleName))
+                throw new ArgumentException("argument is null or empty", nameof(moduleName));
+
             moduleName = moduleName.ToUpperInvariant();
-            if (_moduleMap.ContainsKey(moduleName)) return _moduleMap[moduleName];
+
+            lock (_moduleMap)
+            {
+                if (_moduleMap.ContainsKey(moduleName)) return _moduleMap[moduleName];
+            }
+
 
             var list = new List<IAuditProvider>();
             var providers = Providers;
@@ -100,13 +138,17 @@ namespace Core.Audit.Configuration
             foreach (var provider in providers.Values)
             {
                 if (provider.Modules.Count == 0) list.Add(provider.Provider);
-                else if(provider.Modules.Contains(moduleName)) list.Add(provider.Provider);
+                else if (provider.Modules.Contains(moduleName)) list.Add(provider.Provider);
             }
 
-            if (list.Count > 0) _moduleMap[moduleName] = list;
+            lock (_moduleMap)
+            {
+                if (list.Count > 0) _moduleMap[moduleName] = list;
+            }
 
             return list;
         }
+
         private readonly Dictionary<string, List<IAuditProvider>> _moduleMap = new Dictionary<string, List<IAuditProvider>>();
     }
 }
